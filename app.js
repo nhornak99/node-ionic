cdf65a9c141c83f3c5802fa9587811525c46f717
Noah Hornak <nhornak99@gmail.com>
2019-03-26 23:35:46 -0400

    Added cors and changed route to accept app name

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const { exec } = require("child_process");
+var cors = require("cors");
 const app = express();
-app.listen(3000, () => {
+app.use(cors({ origin: ["http://localhost:3000"] }));
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.listen(3001, () => {
   console.log("Listening...");
 });
 
-app.get("/", (req, res) => {
-  exec("ionic start myApp blank --no-link --no-git", (err, stdout, stderr) => {
-    if (err) console.log(`error: ${err}`);
+app.post("/create", (req, res) => {
+  const { appName } = req.body;
+  exec(
+    `ionic start ${appName} blank --no-link --no-git`,
+    (err, stdout, stderr) => {
+      console.log(`stderr: ${stderr}`);
+      if (err) {
+        console.log(`error: ${err}`);
+        return res.status(400).send();
+      } else {
+        return res.status(201).send();
+      }
 
-    // console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
+      // console.log(`stdout: ${stdout}`);
+    }
+  );
 });
